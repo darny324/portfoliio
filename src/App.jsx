4c9df6ad4d197504474a6dcefadba26f8ed0af68
Khant Sin Kyal <kyalkhantsin@gmail.com>
2024-06-23 15:53:47 +0630

    - remove something

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,8 @@ import { AnimatePresence} from 'framer-motion';
 function App() {
   const location = useLocation();
   const {hash, pathname, search} = location;
-  const doc = useRef(document);
 
   
-  if ( pathname === '/home' || pathname === '/')
-  {
-    console.log(pathname);
-    doc.current.body.style = `
-    background-image: url('../src/assets/images/tech.jpg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover
-    `;
-  }
-  else 
-  {
-    doc.current.body.style = `
-    background-color: #010312;
-    `;
-  }
   return (
     <div>
       <div className="flex flex-col relative">
